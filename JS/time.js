58571305d2418d0b2926fcba5740b2259c586795
Nayan sai <nayansaimalapati2002@gmail.com>
2024-11-19 17:39:58 +0530

    Update time.js

--- JS/time.js
+++ JS/time.js
@@ -1,10 +1,8 @@
+// Time and Date function
 function updateDateTime() {
     var dt = new Date();
     document.getElementById("datetime").innerHTML = dt.toLocaleString();
 }
-
 // Call `updateDateTime` every second to update the time
 setInterval(updateDateTime, 1000);
-
-// Initial call to display the time immediately
 updateDateTime();
